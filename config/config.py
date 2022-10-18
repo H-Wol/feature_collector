7ef2f0700d7691f1efe48141a0a2dc865af4fa40
H_Wall <hunyoon435@gmail.com>
2022-10-19 00:10:58 +0900

    add func config

--- config/config.py
+++ config/config.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import configparser
+from datetime import datetime
 
 
 class ConfigMgr:
@@ -23,7 +24,7 @@ class ConfigMgr:
     def open_config(self):
         try:
             self.config = configparser.ConfigParser()
-            self.config.read('config.ini')
+            self.config.read(self.config_file)
         except Exception as e:
             raise e
 
@@ -34,10 +35,28 @@ class ConfigMgr:
             err_msg = f"set the {name} enviroment variable"
             raise err_msg
 
-    def get_config(self, name, name2):
+    def get_config2(self, name, name2):
         try:
             return self.config[name][name2]
         except KeyError:
             err_msg = f"set the {name}/{name2} enviroment variable"
             raise err_msg
 
+    def get_tried_cnt(self):
+        try:
+            now_tried_cnt  = int(self.config["VT_VALUE"]["tried"])
+            config_date = self.config["VT_VALUE"]["today"]
+            today = datetime.utcnow().strftime('%Y-%m-%d')
+            if config_date != today:
+                self.config["VT_VALUE"]["tried"] = str(0)
+                self.config["VT_VALUE"]["today"] = today
+                return 0
+            now_tried_cnt = now_tried_cnt + 1
+            self.config["VT_VALUE"]["tried"] = str(now_tried_cnt)
+            return now_tried_cnt
+        except Exception as e:
+            err_msg = f"set the VT_VALUE enviroment variable"
+            raise err_msg
+        finally:
+            with open(self.config_file, 'w', encoding='utf-8') as configfile:
+                self.config.write(configfile)
