d479865a9cfd7ecadaa0bf41711c54efb7b142b7
H_Wall <hunyoon435@gmail.com>
2022-10-20 00:14:42 +0900

    fix file_manager

--- file_manager.py
+++ file_manager.py
@@ -2,17 +2,18 @@ import logging
 import traceback
 import pandas as pd
 
+
 def get_data_from_file(file):
     logger = logging.getLogger()
     try:
-        with pd.read_csv(file  , sep=',',header=0, encoding='utf-8') as df:
-            parsed_data = df.groupby("분류")['IOC'].apply(list).to_dict()
-            return parsed_data
-        
+        df =  pd.read_csv(file, sep=',', header=0, encoding='utf-8')
+        parsed_data = df.groupby("분류")['IOC'].apply(list).to_dict()
+        return parsed_data
+
     except Exception as e:
         logger.error(e)
         logger.error(traceback.format_exc())
-    
-    
-if __name__=="__main__":
-    get_data_from_file("FamousSparrow-ioc.csv")
+
+
+if __name__ == "__main__":
+    print(get_data_from_file("IOCS/FamousSparrow-ioc.csv"))
