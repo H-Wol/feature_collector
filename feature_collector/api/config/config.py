4856a52ef22b1b77b55521525fa26f6537382e3a
H_Wall <hunyoon435@gmail.com>
2022-10-25 01:09:42 +0900

    fix config 변경 사항 저장 관련 코드 수정

--- feature_collector/api/config/config.py
+++ feature_collector/api/config/config.py
@@ -43,19 +43,24 @@ class ConfigMgr:
             raise err_msg
 
     def get_tried_cnt(self):
+        """
+        VirusTotal API의 일일 요청 횟수 관련 함수
+        """
         try:
-            now_tried_cnt = int(self.config["VT_VALUE"]["tried"])
-            config_date = self.config["VT_VALUE"]["config_date"]
+            now_tried_cnt = int(self.config["VT_VALUE"]["tried"]) # 요청한 횟수
+            print(now_tried_cnt)
+            config_date = self.config["VT_VALUE"]["config_date"] # 기준 날짜
             today = datetime.utcnow().strftime('%Y-%m-%d')
-            if config_date != today:
-                self.config["VT_VALUE"]["tried"] = str(1)
-                self.config["VT_VALUE"]["config_date"] = today
+            if config_date != today: # 금일이 기준날짜와 다를경우
+                self.config["VT_VALUE"]["tried"] = str(1) #요청한 횟수 초기화
+                self.config["VT_VALUE"]["config_date"] = today #기준 날짜를 금일로 변경
                 return 1
-            now_tried_cnt = now_tried_cnt + 1
-            self.config["VT_VALUE"]["tried"] = str(now_tried_cnt)
-            with open(self.config_file, 'w', encoding='utf-8') as configfile:
-                self.config.write(configfile)
+            now_tried_cnt = now_tried_cnt + 1 #요청 횟수 추가
+            self.config["VT_VALUE"]["tried"] = str(now_tried_cnt) #요청 횟수 기록
             return now_tried_cnt
         except Exception as e:
             err_msg = f"set the VT_VALUE enviroment variable"
             raise err_msg
+        finally:
+            with open(self.config_file, 'w', encoding='utf-8') as configfile: # 변경 사항 저장
+                self.config.write(configfile)  
